1866f541574a2c69b85aa5bad746affc032bfda2
Paul <qamunyamaina950@gmail.com>
2025-03-04 21:43:21 +0300

    stored the input in the title, author and pages in the object array after creating them in the constructor

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 let dialog = document.querySelector(".dialog");
 let addBook = document.querySelector('.add-book');
 let closeDialog = document.querySelector('.close-dialog');
+let title = document.querySelector('.title');
+let author = document.querySelector('.author');
+let pages = document.querySelector('.pages');
+const library = [];
 
 addBook.addEventListener('click', function(){
     dialog.showModal();
-    // dialog.style = 'display: block; gap: 1rem;';
+    title.value ='';
+    author.value = '';
+    pages.value = '';
 });
 closeDialog.addEventListener ('click', function(){
+    let titleValue = title.value;
+    let authorValue = author.value;
+    let pagesValue = pages.value;
+    addToLibrary(titleValue, authorValue, pagesValue);
+    console.log(library);
     dialog.close();
 });
 
 
-const library = [];
 
-function Book(name, author, pages){
 
+function Book(Title, Author, Pages){
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages = Pages;
 }
 
-function addToLibrary(){
-
+function addToLibrary(Title, Author, Pages){
+    library.push(new Book(Title, Author, Pages));
 }
+
+
 
 
 
